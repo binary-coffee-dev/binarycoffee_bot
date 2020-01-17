2a6f48aa0e7930b188ea2bcb9991243ab7005651
Raúl Rubén Castro Rivero <rrubencr@estudiantes.uci.cu>
2020-01-17 17:12:00 -0500

    First Commit

---
+++ bot.py
@@ -0,0 +1,45 @@
+from env import TOKEN, CHANNEL, RSS_FEED, REFRESH_TIME
+import telegram
+import time
+import requests
+import json
+
+def load():
+    file = open('data.json', 'r')
+    data = file.read().replace("'", '"')
+    ret = []
+    if len(data) > 0:
+        ret = json.loads(data)
+    file.close()
+    return ret
+
+last_posts = load()
+
+def save():
+    with open('data.json', 'w+') as database:
+        database.write(str(last_posts))
+
+def send(msg, chat_id=CHANNEL, token=TOKEN):
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=chat_id, text=msg)
+
+while True:
+    time.sleep(REFRESH_TIME)
+    try:
+        data = requests.get(RSS_FEED)
+        posts = json.loads(data._content)
+        
+        if last_posts == []:
+            for i in posts['items']:
+                send('{}\n{}'.format(i['summary'], i['url']))
+        
+        elif posts != last_posts:
+            for i in posts['items']:
+                if i not in last_posts['items']:
+                    send('{}\n{}'.format(i['summary'], i['url']))
+        
+        if posts != last_posts:
+            last_posts = posts
+            save()
+    except:
+        pass

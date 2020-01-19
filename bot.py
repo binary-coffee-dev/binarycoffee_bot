dac7f736053ceaaecd80cfc59916324d9e437dfd
Raúl Rubén Castro Rivero <rrubencr@estudiantes.uci.cu>
2020-01-19 16:03:17 -0500

    FIXING

--- bot.py
+++ bot.py
@@ -20,13 +20,13 @@ def save():
     with open('data.json', 'w+') as database:
         database.write(str(last_posts))
 
-def send(msg, chat_id=CHANNEL, token=os.enviroment['TOKEN']):
+def send(msg, chat_id=CHANNEL, token=os.environ['TOKEN']):
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=chat_id, text=msg)
 
 while True:
     try:
-        data = requests.get(os.enviroment['RSS_FEED'])
+        data = requests.get(os.environ['RSS_FEED'])
         posts = json.loads(data._content)
         
         if last_posts == []:

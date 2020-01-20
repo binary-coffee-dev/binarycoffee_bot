9820ea8b818ac5aac4976cc60bb2c2c6992c9aaf
Raúl Rubén Castro Rivero <rrubencr@estudiantes.uci.cu>
2020-01-20 16:55:55 -0500

    ADD Facebook class

--- social.py
+++ social.py
@@ -1,5 +1,6 @@
 import os
 import telegram
+from facepy import GraphAPI
 
 class Telegram:
     def __init__(self):
@@ -9,3 +10,15 @@ class Telegram:
     def post(self, msg):
         bot = telegram.Bot(token=self.token)
         bot.sendMessage(chat_id=self.chat_id, text=msg)
+
+class Facebook:
+    def __inti__(self):
+        self.api = os.environ['FACEBOOK_KEY']
+        self.group = os.environ['FACEBOOK_GROUP']
+        self.graph = GraphAPI(self.api)
+
+    def post(self, msg):
+        if self.api == 'None' or self.group == 'None':
+            return
+        message = msg
+        self.graph.post(path=str(self.group) + '/feed', message=message)

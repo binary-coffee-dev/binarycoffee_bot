437103e82c3fa85268037bbcb456f1b758b45be3
Raúl Rubén Castro Rivero <rrubencr@estudiantes.uci.cu>
2020-01-22 12:00:18 -0500

    FIX errors

--- social.py
+++ social.py
@@ -1,24 +1,11 @@
 import os
 import telegram
-from facepy import GraphAPI
 
 class Telegram:
     def __init__(self):
         self.token = os.environ['TOKEN']
         self.chat_id = os.environ['CHANNEL']
+        self.bot = telegram.Bot(token=self.token)
 
     def post(self, msg):
-        bot = telegram.Bot(token=self.token)
-        bot.sendMessage(chat_id=self.chat_id, text=msg)
-
-class Facebook:
-    def __inti__(self):
-        self.api = os.environ['FACEBOOK_KEY']
-        self.group = os.environ['FACEBOOK_GROUP']
-        self.graph = GraphAPI(self.api)
-
-    def post(self, msg):
-        if self.api == 'None' or self.group == 'None':
-            return
-        message = msg
-        self.graph.post(path=str(self.group) + '/feed', message=message)
+        self.bot.sendMessage(chat_id=self.chat_id, text=msg)

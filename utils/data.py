6f336f5bffc42e43b8f98274160cd89d7961aa9b
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2020-02-21 15:38:26 +0100

    Create mongodb adapter generation (#1)

--- utils/data.py
+++ utils/data.py
@@ -1,5 +1,15 @@
 from utils.data_adapters.json_adapter import JsonAdapter
+from utils.data_adapters.mongodb_adapter import MongodbAdapter
 
 
-def get_data_adapter(strategy='JsonAdapter', data_path=''):
+def get_data_adapter(strategy='JsonAdapter',
+                     data_path='',
+                     database_url=None,
+                     database_port=None,
+                     database_name=None):
+    if strategy == 'MongodbAdapter':
+        database_name = 'bc_db' if database_name is None else database_name
+        database_url = 'localhost' if database_url is None else database_url
+        database_port = 27017 if database_port is None else database_port
+        return MongodbAdapter(database_url=database_url, database_port=database_port, database_name=database_name)
     return JsonAdapter(data_storage_path=data_path)
